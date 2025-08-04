646060373e35686619756db73c7bb350480c399c
kLaz3r <klazer220033@gmail.com>
2025-08-04 08:23:42 +0300

    bigger text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
             <section id="Hero" className="text-foreground min-h-screen pt-20">
                 <div className="fixed right-0 bg-[url(/halfton-90.svg)] bg-cover w-1/2 min-h-screen -z-50"></div>
                 <div className="container md:flex-row mx-auto flex flex-col items-start md:items-center justify-center min-h-[calc(100vh-5rem)]">
-                    <div className="flex px-6 py-6 w-full gap-4 flex-col items-start justify-start">
+                    <div className="flex px-6 py-6 w-full gap-6 flex-col items-start justify-start">
                         <h1 className="text-5xl/14 md:text-7xl/20 font-italiana">
                             Hi, I am{" "}
                             <span className="text-brand-primary">Stefan</span>
                             <br />
                             Web Developer <br />& Designer
                         </h1>
-                        <h2 className="font-sora text-base mr-12 md:max-w-sm">
+                        <h2 className="font-sora text-xl mr-12 md:max-w-sm">
                             I build beautiful and intuitive web applications
                             from concept to deployment.
                         </h2>
