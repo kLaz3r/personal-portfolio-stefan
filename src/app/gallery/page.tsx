b2f6b1a871e314dd27155e34ad9039f538080373
kLaz3r <klazer220033@gmail.com>
2025-08-14 08:15:04 +0300

    done the photo gallery page and added the lightbox

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,77 @@
 "use client";
 
+import Image from "next/image";
+import {
+    MasonryPhotoAlbum,
+    RenderImageContext,
+    RenderImageProps,
+} from "react-photo-album";
+import "react-photo-album/masonry.css";
+import "yet-another-react-lightbox/styles.css";
+
+import { photos } from "../../data/photos";
+
+import Lightbox from "yet-another-react-lightbox";
+
+import NextJsImageLightbox from "@/components/NextJsImageLightbox";
+import React from "react";
+
+function renderNextImage(
+    { alt = "", title, sizes }: RenderImageProps,
+    { photo, width, height }: RenderImageContext
+) {
+    return (
+        <div
+            style={{
+                width: "100%",
+                position: "relative",
+                aspectRatio: `${width} / ${height}`,
+            }}
+        >
+            <Image
+                fill
+                className="rounded-md"
+                src={photo}
+                alt={alt}
+                title={title}
+                sizes={sizes}
+                placeholder={"blurDataURL" in photo ? "blur" : undefined}
+            />
+        </div>
+    );
+}
+
 const GalleryPage = () => {
-    return <div>secs</div>;
+    const [index, setIndex] = React.useState(-1);
+    return (
+        <>
+            <section id="about" className="min-h-screen pt-20 pb-12">
+                <div className="container mx-auto w-full min-h-[calc(100vh-5rem)] px-6 py-6">
+                    <MasonryPhotoAlbum
+                        photos={photos}
+                        render={{ image: renderNextImage }}
+                        defaultContainerWidth={1200}
+                        onClick={({ index: current }) => setIndex(current)}
+                        sizes={{
+                            size: "1168px",
+                            sizes: [
+                                {
+                                    viewport: "(max-width: 1200px)",
+                                    size: "calc(100vw - 32px)",
+                                },
+                            ],
+                        }}
+                    />
+                </div>
+                <Lightbox
+                    index={index}
+                    slides={photos}
+                    open={index >= 0}
+                    close={() => setIndex(-1)}
+                    render={{ slide: NextJsImageLightbox }}
+                />
+            </section>
+        </>
+    );
 };
 export default GalleryPage;
---
+++ src/components/NextJsImageLightbox.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import {
+    isImageFitCover,
+    isImageSlide,
+    RenderSlideProps,
+    Slide,
+    SlideImage,
+    useLightboxProps,
+    useLightboxState,
+} from "yet-another-react-lightbox";
+
+function isNextJsImage(slide: Slide): slide is SlideImage & {
+    width: number;
+    height: number;
+    blurDataURL?: string;
+} {
+    return (
+        isImageSlide(slide) &&
+        typeof slide.width === "number" &&
+        typeof slide.height === "number"
+    );
+}
+
+export default function NextJsImageLightbox({
+    slide,
+    offset,
+    rect,
+}: RenderSlideProps) {
+    const {
+        on: { click },
+        carousel: { imageFit },
+    } = useLightboxProps();
+
+    const { currentIndex } = useLightboxState();
+
+    const cover = isImageSlide(slide) && isImageFitCover(slide, imageFit);
+
+    if (!isNextJsImage(slide)) return undefined;
+
+    const width = !cover
+        ? Math.round(
+              Math.min(rect.width, (rect.height / slide.height) * slide.width)
+          )
+        : rect.width;
+
+    const height = !cover
+        ? Math.round(
+              Math.min(rect.height, (rect.width / slide.width) * slide.height)
+          )
+        : rect.height;
+
+    return (
+        <div style={{ position: "relative", width, height }}>
+            <Image
+                fill
+                alt=""
+                src={slide}
+                loading="eager"
+                draggable={false}
+                placeholder={slide.blurDataURL ? "blur" : undefined}
+                style={{
+                    objectFit: cover ? "cover" : "contain",
+                    cursor: click ? "pointer" : undefined,
+                }}
+                sizes={`${Math.ceil((width / window.innerWidth) * 100)}vw`}
+                onClick={
+                    offset === 0
+                        ? () => click?.({ index: currentIndex })
+                        : undefined
+                }
+            />
+        </div>
+    );
+}
