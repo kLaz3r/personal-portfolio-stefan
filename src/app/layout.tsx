7f6c9ccfe488cb74177ae56ef980625fb4225e3d
klaz3r <klazer220033@gmail.com>
2025-08-16 09:30:37 +0300

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
+                <meta
+                    name="apple-mobile-web-app-title"
+                    content="StefanWebDev"
+                />
                 <script
                     dangerouslySetInnerHTML={{
                         __html: `
