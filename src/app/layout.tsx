2fdb592cac5a72eb695660089775f74353ca6826
Stefan Nasturas <stefan@example.com>
2026-04-30 08:52:17 +0300

    ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta
-          name="apple-mobile-web-app-title"
-          content="StefanWebDev"
-        />
+        <meta name="apple-mobile-web-app-title" content="StefanWebDev" />
         <link rel="canonical" href="https://stefan-nasturas.dev" />
         <Script id="theme-initializer" strategy="beforeInteractive">
           {`
@@ -52,20 +49,17 @@ export default function RootLayout({
         {/* Matomo */}
         <Script id="matomo-tracking" strategy="afterInteractive">
           {`
-            var _paq = window._paq = window._paq || [];
-            _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-            _paq.push(["setCookieDomain", "*.stefannasturas.vercel.app"]);
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="//matomo.stefann.duckdns.org/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '1']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true;
-              g.src=u+'matomo.js';
-              s.parentNode.insertBefore(g,s);
-            })();
+             var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.stefann.duckdns.org/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
           `}
         </Script>
         <noscript>
